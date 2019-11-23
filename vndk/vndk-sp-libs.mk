58f09b21c9f6b281a5f8f7a0a427b34c4ff6d53f
faust93 <monumentum@gmail.com>
2019-11-23 12:03:16 +0200

    though BOARD_VNDK is defined, let's keep vndk-sp libs up-to-date just in case

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -1,5 +1,4 @@
 VNDK_SP_LIBRARIES := \
-    android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.mapper@2.0 \
     android.hardware.graphics.mapper@2.1 \
     android.hardware.graphics.mapper@3.0 \
@@ -7,7 +6,6 @@ VNDK_SP_LIBRARIES := \
     android.hardware.graphics.common@1.1 \
     android.hardware.graphics.common@1.2 \
     android.hardware.renderscript@1.0 \
-    android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
     android.hidl.memory.token@1.0 \
     android.hidl.safe_union@1.0 \
@@ -23,7 +21,6 @@ VNDK_SP_LIBRARIES := \
     libc++ \
     libcompiler_rt \
     libcutils \
-    libft2 \
     libhardware \
     libhidlbase \
     libhidlmemory \
@@ -34,10 +31,8 @@ VNDK_SP_LIBRARIES := \
     libjsoncpp \
     liblzma \
     libprocessgroup \
-    libpng \
     libunwind \
     libunwindstack \
     libutils \
     libutilscallstack \
-    libdexfile \
     libz
