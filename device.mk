a4a48991fa4718ddbacff1dcd998016df6463229
faust93 <monumentum@gmail.com>
2019-12-05 19:30:29 +0200

    Build power stats hal

--- device.mk
+++ device.mk
@@ -394,7 +394,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-grus
+    android.hardware.power@1.2-service-grus \
+    android.hardware.power.stats@1.0-service.mock
 
 # Permission
 PRODUCT_COPY_FILES += \
